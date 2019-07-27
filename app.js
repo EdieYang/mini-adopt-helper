3501a89b87b3230e0c638ac779a6c757bccc933f
lookluke <haojie_look@163.com>
2019-07-27 18:09:58 +0800

    更新线上环境

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ App({
     authorized: false,
     userInfo: null,
     userId: '',
-    // requestUrlCms: 'https://www.linchongpets.com/lpCms',
-    // requestUrlWechat: 'https://www.linchongpets.com/lpWechat',
-    requestUrlCms: 'http://localhost:8093/lpCms',
-    requestUrlWechat: 'http://localhost:8094/lpWechat'
+    requestUrlCms: 'https://www.linchongpets.com/lpCms',
+    requestUrlWechat: 'https://www.linchongpets.com/lpWechat',
+    // requestUrlCms: 'http://localhost:8093/lpCms',
+    // requestUrlWechat: 'http://localhost:8094/lpWechat'
   },
   onLaunch: function(options) {
     console.log("linkpets.miniapp.launching.....")
